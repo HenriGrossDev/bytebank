8b33bbe4ee9c1614ae61cfd17c6a51144969e410
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2022-04-06 19:36:19 -0300

    Adicionando dados interpolados no componente html e limpandos valores após emitir evento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bitebank1';
+  destino!:number;
+  valor!:number;
+  transferencia:any;
 
   transferir($event: any){
-    console.log($event)
+    console.log($event);
+    this.transferencia = $event;
   }
 }
