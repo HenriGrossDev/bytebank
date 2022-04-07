b2a5aaf0d44b7ae0de6c44794307049fa5352d51
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2022-04-06 21:19:13 -0300

    Enviando os dados para o componente extrato

--- src/app/extrato/extrato.component.ts
+++ src/app/extrato/extrato.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-extrato',
@@ -7,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExtratoComponent implements OnInit {
 
+  @Input() transferencia: any;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
 
 }
